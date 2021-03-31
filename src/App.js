d12ea9210de3c274624799ee94e37f601d047b66
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-03-31 13:03:04 +0600

    created a  contst usestate for sum numbers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import Countdown from "./components/Countdown/Countdown";
+const App = () => {
+  const [a, setA] = useState(randomNumber(50));
+  const [b, setB] = useState(randomNumber(50));
+  const [answer, setAnswer] = useState("?");
+  const [countdown, setCountdown] = useState(20);
+  function randomNumber(limit) {
+    return Math.round(Math.random() * limit);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {a} + {b} = {answer}
+      <Countdown countdown={countdown} setCountdown={setCountdown} />
+      <input type="number" value={answer} onChange={({ target }) => setAnswer(target.value)} />
+      <button onClick={() => checkAnswer()}>Ok</button>
     </div>
   );
 }
-
 export default App;
